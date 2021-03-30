21272a26981cb09a2b2d2d9475eb855808a7eef7
joshivinit2018 <joshivinit2018@gmailcom>
2021-03-30 22:33:58 +0530

    [vinit]:add multiple contact details

--- src/main/java/com/bridgelabz/addressbook.java
+++ src/main/java/com/bridgelabz/addressbook.java
@@ -1,8 +1,150 @@
 package com.bridgelabz;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class addressbook {
+
+    private final String firstName;
+    private final String lastName;
+    private final String address;
+    private final String city;
+    private final String state;
+    private final int zip;
+    private final String phone;
+    private final String email;
+
+    public addressbook(String firstName, String lastName, String address, String city,
+                       String state, int zip, String phone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String toString() {
+        return "\tFirst Name: " + firstName + "\n\tLast Name: " + lastName + "\n\tAddress: " +
+                address + "\n\tCity: " + city + "\n\tState: " + state + "\n\tZIP: " + zip +
+                "\n\tPhone: " + phone + "\n\temail: " + email;
+    }
+
+    public static String[] main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Array Declaration
+        addressbook[] content = new addressbook[1];
+        //Instance variables
+        int entries, zip;
+        String first, last, address, city, state, phone, email, choice;
+
+        //Asking User for 1 or more Entries
+        System.out.println("How many new Entries do you require in AddressBook: ");
+        entries = sc.nextInt();
+        // Initialising Array
+        if ( entries > 0)
+            content = new addressbook[entries];
+        else {
+            System.out.println("Must Create atleast 1 Entry");
+            System.exit(0);
+        }
+
+        System.out.println("\t*****Address Book*****\t");
+        for ( int i = 0 ; i < entries ; i++ ) {
+            System.out.println("Entry: "+(i+1));
+            System.out.print("First name? ");
+            first = sc.next();
+            System.out.print("Last name? ");
+            last = sc.next();
+            System.out.print("Address? ");
+            address = sc.next();
+            System.out.print("City? ");
+            city = sc.next();
+            System.out.print("State? ");
+            state = sc.next();
+            System.out.print("ZIP? ");
+            zip = sc.nextInt();
+            System.out.print("Phone? ");
+            phone = sc.next();
+            System.out.print("Email? ");
+            email = sc.next();
+            content[i] = new addressbook(first, last, address, city, state, zip, phone, email);
+        }
+
+        for ( int i = 0 ; i < entries ; i++ ) {
+            System.out.println("\t*****Address Book*****\t");
+            System.out.println("Entry: "+(i+1));
+            System.out.println(content[i]);
+        }
+
+
+
+        System.out.println("Do you want to edit any Entries u just created? (y/n)");
+        choice = sc.next();
+        if (choice.equals("Y") || choice.equals("y")) {
+            String name;
+            System.out.println("Please enter the first name of Person u want to edit: ");
+            name = sc.next();
+            for (int i = 0; i < entries; i++) {
+                if (Objects.equals(content[i].firstName, name)) {
+                    System.out.print("First name? ");
+                    first = sc.next();
+                    System.out.print("Last name? ");
+                    last = sc.next();
+                    System.out.print("Address? ");
+                    address = sc.next();
+                    System.out.print("City? ");
+                    city = sc.next();
+                    System.out.print("State? ");
+                    state = sc.next();
+                    System.out.print("ZIP? ");
+                    zip = sc.nextInt();
+                    System.out.print("Phone? ");
+                    phone = sc.next();
+                    System.out.print("Email? ");
+                    email = sc.next();
+                    content[i] = new addressbook(first, last, address, city, state, zip, phone, email);
+                } else
+                    System.out.println("Person Not found at Entry " + (i + 1) + " with Person FirstName: " + name);
+            }
+            for (int i = 0; i < entries; i++) {
+                System.out.println("\t*****Address Book*****\t");
+                System.out.println("(Edited)Entry: " + (i + 1));
+                System.out.println(content[i]);
+            }
+        }
+        else
+            System.out.println("Thank You!!");
+
+        System.out.println("Do you want to delete any Entries u just created? (y/n)");
+        choice = sc.next();
+        if (choice.equals("Y") || choice.equals("y")) {
+            addressbook[] removedArray = new addressbook[content.length-1];
+            String name;
+            System.out.println("Please enter the first name of Person u want to edit: ");
+            name = sc.next();
+            for (int i = 0, k=0; i < entries; i++) {
+                if (Objects.equals(content[i].firstName, name)) {
+                    continue;
+                }
+                removedArray[k++] = content[i];
+            }
+            entries = entries-1;
+            System.arraycopy(removedArray, 0, content, 0, entries);
+            for (int i = 0; i < entries; i++) {
+                System.out.println("\t*****Address Book*****\t");
+                System.out.println("Entry: " + (i + 1));
+                System.out.println(content[i]);
+            }
+        }
+        else
+            System.out.println("******Thank You******");
+    }
+
+
+
 
         public String[] Person() {
             Scanner input = new Scanner(System.in);
@@ -147,4 +289,5 @@ public class addressbook {
             }
             System.out.println("******THANK YOU******");
         }
+
 }
