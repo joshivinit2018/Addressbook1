aeff8642f640d7aad0f0c6b69cc5601d7e9e9afc
joshivinit2018 <joshivinit2018@gmailcom>
2021-03-30 21:30:44 +0530

    [vinit]:add add new contact

--- src/main/java/com/bridgelabz/addressbook.java
+++ src/main/java/com/bridgelabz/addressbook.java
@@ -1,5 +1,7 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class addressbook {
     private String first_name;
     private String last_name;
@@ -21,6 +23,39 @@ public class addressbook {
         this.email = email;
     }
 
+    public addressbook() {
+
+    }
+
+    public void Person() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your First Name");
+        String first_name = input.next();
+        System.out.println("Enter your Last Name");
+        String last_name = input.next();
+        System.out.println("Enter your House number");
+        String house_no = input.next();
+        System.out.println("Enter your City name");
+        String city = input.next();
+        System.out.println("Enter your State name");
+        String state = input.next();
+        System.out.println("Enter your Pin Code");
+        String pin_code = input.next();
+        System.out.println("Enter your Phone number");
+        String phone_number = input.next();
+        System.out.println("Enter your e-mail address");
+        String email = input.next();
+        System.out.println("First Name: " + first_name);
+        System.out.println("Last Name: " + last_name);
+        System.out.println("House Number: " + house_no);
+        System.out.println("City: " + city);
+        System.out.println("State: " + state);
+        System.out.println("Pin Code: " + pin_code);
+        System.out.println("Phone Number: " + phone_number);
+        System.out.println("E-Mail: " + email);
+    }
+
+
     public static void main(String[] args) {
         System.out.println("WELCOME to Address Book Program");
         addressbook entry = new addressbook("Vinit", "Joshi", 102, "kapshi", "maharashtra", 416218, "5465651463", "joshivinit@gmail.com");
@@ -32,5 +67,7 @@ public class addressbook {
         System.out.println("Pin code: " + entry.pin_code);
         System.out.println("Phone number: " + entry.phone_number);
         System.out.println("E-Mail: " + entry.email);
+        addressbook contact = new addressbook();
+        contact.Person();
     }
 }
